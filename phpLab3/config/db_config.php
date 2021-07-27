d4f378bc182007bdf83434b2d797f11bbfd9f276
ArtemSukhorukikh <53040357+ArtemSukhorukikh@users.noreply.github.com>
2021-07-27 23:00:30 +0300

    Delete phpLab3 directory

--- phpLab3/config/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'host' => 'localhost',
-	'port' => '5432',
-	'name' => 'phpLab2',
-	'user' => 'postgres',
-	'password' => '',
-];
--- phpLab3/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require 'Db.php';
-
-$db = new Db();
-
-$db->loadDataFromXml('xml/books.xml');
-$db->saveDataToJson('books.json');
